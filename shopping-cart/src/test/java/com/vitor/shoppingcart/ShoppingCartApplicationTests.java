1ce97804921a7a375e1e60062e91d29a60f06f50
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-25 18:01:56 -0300

    add shopping cart project

---
+++ shopping-cart/src/test/java/com/vitor/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
