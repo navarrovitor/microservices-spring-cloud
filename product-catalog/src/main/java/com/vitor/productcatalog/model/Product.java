332e9bb60d2da9285a62b1e4ae61a0d21b94ec26
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-25 16:18:38 -0300

    add model package with product class

---
+++ product-catalog/src/main/java/com/vitor/productcatalog/model/Product.java
@@ -0,0 +1,42 @@
+package com.vitor.productcatalog.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "product")
+public class Product {
+
+  @Id
+  private Long id;
+  private String name;
+  private Integer amount;
+
+  // the implementation of getters and settersnot is not necessary since we're
+  // using Spring Boot
+  // todo: implement all args constructor
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Integer amount) {
+    this.amount = amount;
+  }
+
+}
