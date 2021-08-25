92b583af23a888cdee4ebc85c0f406d29c6a8308
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-25 16:56:25 -0300

    delete some files to resolve bug from zero

--- product-catalog/src/main/java/com/vitor/productcatalog/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vitor.productcatalog.repository;
-
-import com.vitor.productcatalog.model.Product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
